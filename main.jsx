f5541afca07a4cc53af843f7d0026c6abeb28a7b
JackieDark7 <roshangtanna@gmail.com>
2026-03-18 14:06:11 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import KavyaWebsite from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KavyaWebsite />
-  </React.StrictMode>,
-)
